a3603dc22e1b771ae24b2f91407f249d82834dee
gabeadmin <gg.godfreygabriel@gmail.com>
2024-07-13 14:28:46 -0400

    rework cat script to flip sprite

--- scripts/cat.gd
+++ scripts/cat.gd
@@ -1,33 +1,38 @@
 extends CharacterBody2D
 
 const speed = 60
-var direction = {}
-
-
-@export var player: Node2D
-@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
-@onready var animated_sprite = $AnimatedSprite2D
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(_delta: float) -> void:
-	# add gravity
-	var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-	
-	
-	var idle = nav_agent.is_navigation_finished()
-	
-	if idle == true:
-		direction = 0
+#this var needs to be linked with the player node in the inspector
+@export var player: Node2D
+
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	#add gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+#path finder stuff shamlessly copy pasted
+	var direction = to_local(nav_agent.get_next_path_position()).normalized()
+	#move
+	velocity = direction * speed
+	if velocity.x > 0:
+		animated_sprite.flip_h = false
+		animated_sprite.play("run")
+	elif velocity.x < 0:
+		animated_sprite.flip_h = true
+		animated_sprite.play("run")
 	else:
-		direction = to_local(nav_agent.get_next_path_position()).normalized()
-		velocity = direction * speed
-		move_and_slide()
-
-func makepath() -> void:
-	nav_agent.target_position = player.global_position
+		animated_sprite.play("idle")
 	
-	print(direction)
+	move_and_slide()
 
-
+#this function updates the pathfinder 10 times/sec
 func _on_timer_timeout() -> void:
 	makepath()
+
+#more pathfinder stuff
+func makepath() -> void:
+	nav_agent.target_position = player.global_position
