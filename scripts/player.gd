e4e351ecacd4042342a30dce1097891b7689bf7e
gabeadmin <gg.godfreygabriel@gmail.com>
2024-07-14 15:04:57 -0400

    cleanup some code

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @export var attacking = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -25,9 +25,11 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("a", "d")
-	if direction:
+	if direction and attacking == false:
 		velocity.x = direction * SPEED
+		animated_sprite.play("run")
 	else:
+		animated_sprite.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	#flip sprite when movie left or right
@@ -37,14 +39,15 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = true
 	
 	#play animations
-	if is_on_floor():
-		if direction == 0 and !attacking:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
+	#if is_on_floor():
+		#if direction == 0 and !attacking:
+		#	animated_sprite.play("idle")
+		#else:
+			#animated_sprite.play("run")
 
 	move_and_slide()
 
 func attack():
 	attacking = true
 	animated_sprite.play("attack")
+	animated_sprite.set_offset(Vector2(30, 0))
