0aca58767e550d87b385f5a0047d95ae720b6ffb
gabeadmin <gg.godfreygabriel@gmail.com>
2024-07-14 13:29:16 -0400

    remove sleep anim logic
    after hours of debugging, code wasn't working and found an edge case where it made the run animation stutter. will come back later. will work on other stuff for now

--- scripts/cat.gd
+++ scripts/cat.gd
@@ -17,6 +17,13 @@ func _physics_process(delta: float) -> void:
 	#path finder stuff shamlessly copy pasted
 	var direction = to_local(nav_agent.get_next_path_position()).normalized()
 	
+	#flip sprite
+	if velocity.x > 0 and direction:
+		animated_sprite.flip_h = false
+	elif velocity.x < 0 and direction:
+		animated_sprite.flip_h = true
+	
+	var only_once = true
 	#move logic
 	if direction:
 		#if we did not reach the player
@@ -24,28 +31,46 @@ func _physics_process(delta: float) -> void:
 			#we move
 			velocity = direction * speed
 			animated_sprite.play("run")
+			#animated_tree.set("parameters/moving_anims/current_state/input_1/run", 1)
+			#current_anim = RUN
+			#animated_tree["parameters/conditions/is_moving"] = true
+			#animated_tree["parameters/conditions/idle"] = false
+			#animated_tree["parameters/conditions/is_sleeping"] = false
 		#if we did reach the player
-		elif nav_agent.is_navigation_finished() == true:
+		if nav_agent.is_navigation_finished() == true:
 			#we stop moving
 			velocity.y = 0
 			velocity.x = 0
-			animated_sprite.play("idle")
-			#TODO: this code doesn't work. figure out how to swap idle animations...
-			await get_tree().create_timer(5.0).timeout
-			print("waited 5 seconds, sleeping")
-			animated_sprite.sprite_frames.remove_animation("idle")
-			print("animation cleared")
-			animated_sprite.play("sleep")
-			animated_sprite.sprite_frames.get_animation_loop("sleep")
-			animated_sprite.sprite_frames.set_animation_loop("sleep", false)
+			animated_sprite.play("idle")	
+			#TODO: this code doesn't work. figure out how to stop sleeping loop
+			#await get_tree().create_timer(5.0).timeout
+			#for some reason .stop just crashes the whole thing
+			#animated_sprite.pause()
+			#we delete the idle animation
+			#animated_sprite.sprite_frames.remove_animation("idle")
+			# we add it back otherwise the code crashes
+			#animated_sprite.sprite_frames.add_animation("idle")
+			#we manually set the animation we want to play
+			#animated_sprite.set_animation("sleep")
+			#we actually play the animation
+			#animated_sprite.play("sleep")
+				#animated_sprite.pause()
+			#var frame_count = animated_sprite.sprite_frames.get_frame_count("sleep")
+			#only_once = true
+		
 			
+			#while animated_sprite.sprite_frames.get_animation_loop("idle") == true:
+			#	animation_finished()
+			#	break
+			#if animated_sprite.is_playing():
+			#	animated_sprite.sprite_frames.add_animation("idle")
+			#	var frame_count = animated_sprite.sprite_frames.get_frame_count("sleep")
+			#	print(frame_count)
+			#	animated_sprite.sprite_frames.set_animation_loop("sleep", false)
+			#	animated_sprite.play("sleep")
+			#	if frame_count == 8 and animated_sprite.is_playing():
+			#		animated_sprite.pause()
 	move_and_slide()
-
-	#flip sprite
-	if velocity.x > 0 and direction:
-		animated_sprite.flip_h = false
-	elif velocity.x < 0 and direction:
-		animated_sprite.flip_h = true
 
 #this function updates the pathfinder 10 times/sec
 func _on_timer_timeout() -> void:
