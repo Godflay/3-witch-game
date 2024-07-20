65cd05599e0812cbd52074ccd12c1229a6872430
gabeadmin <gg.godfreygabriel@gmail.com>
2024-07-20 09:30:48 -0400

    add audio to menu buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,9 +4,13 @@ class_name main_menu
 @onready var play = $MarginContainer/HBoxContainer/VBoxContainer/PLAY as Button
 @onready var quit = $MarginContainer/HBoxContainer/VBoxContainer/QUIT as Button
 @onready var start_level = preload("res://scenes/game.tscn") as PackedScene
+@onready var click = $click
+@onready var select = $select
 
 
 func _ready():
+	_on_play_mouse_entered()
+	_on_quit_mouse_entered()
 	play.button_down.connect(on_play)
 	quit.button_down.connect(on_quit)
 
@@ -15,3 +19,11 @@ func on_play() -> void:
 
 func on_quit() -> void:
 	get_tree().quit()
+
+
+func _on_play_mouse_entered():
+	select.play("select")
+
+
+func _on_quit_mouse_entered() -> void:
+	select.play("select")
