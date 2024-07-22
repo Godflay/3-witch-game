4c1f01b78fc3d3b6d9e6f5cec5962226eb2fca47
gabeadmin <gg.godfreygabriel@gmail.com>
2024-07-21 20:38:32 -0400

    added a dialogue loop to wizard after receiving campaign info

--- scripts/old_wizard.gd
+++ scripts/old_wizard.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
 var player_in_area = false
+@onready var wizard_met = false
 
 func _process(delta):
 	if player_in_area:
-		if Input.is_action_just_pressed("e"):
-			run_dialogue("wizard_meeting")
+		if Input.is_action_just_pressed("e") and wizard_met == false:
+			wizard_meeting()
+		elif Input.is_action_just_pressed("e") and wizard_met == true:
+			wizard_main_loop()
 
 func run_dialogue(dialogue_string):
 	Dialogic.start(dialogue_string)
@@ -17,3 +20,11 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body:Node2D) -> void:
 	if body.has_method("player"):
 		player_in_area = false
+
+func wizard_meeting():
+	run_dialogue("wizard_meeting")
+	wizard_met = true
+
+func wizard_main_loop():
+	var dialogue_line = randi_range(1,3)
+	Dialogic.start("wizard_loop_" + str(dialogue_line))
