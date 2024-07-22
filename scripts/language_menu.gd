08aa8b4fd7f54bf101e62ad9ffeb711282494e1b
gabeadmin <gg.godfreygabriel@gmail.com>
2024-07-21 23:39:57 -0400

    rework main main

--- scripts/language_menu.gd
+++ scripts/language_menu.gd
@@ -5,22 +5,45 @@ class_name languagemenu
 @onready var french = $MarginContainer/HBoxContainer/VBoxContainer/French as Button
 @onready var german = $MarginContainer/HBoxContainer/VBoxContainer/German as Button
 @onready var back = $MarginContainer/HBoxContainer/VBoxContainer/Exit as Button
+@onready var main_menu = preload("res://scenes/menus/main_menu.tscn") as PackedScene
+@onready var language_menu = preload("res://scenes/menus/language_menu.tscn") as PackedScene
 
-signal exit_language_menu
+@onready var current_language = ""
+#signal exit_language_menu
 
 func _ready():
-    back.button_down.connect(on_exit)
-    set_process(false)
+	back.button_down.connect(on_exit)
+	print("click on back button")
+	#set_process(false)
+
+#func transfer_data(old_scene, new_scene):
+	
+    #old_scene.current_language = new_scene.current_language
+
 
 func on_exit() -> void:
-    exit_language_menu.emit()
-    set_process(false)
+	get_tree().change_scene_to_packed(main_menu)
+	current_language = current_language
+	#transfer_data(language_menu, main_menu)
+	#exit_language_menu.emit()
+	#set_process(false)
 
-func on_english_button_up() -> void:
- TranslationServer.set_locale("en")
+func handle_language_change():
+	if on_english_button_up() == true:
+		current_language = "en"
+		return
+	elif on_french_button_up() == true:
+		current_language = "fr"
+		return
+	elif on_german_button_up() == true:
+		current_language = "de"
+		return
 
-func on_french_button_up() -> void:
- TranslationServer.set_locale("fr")
+func on_english_button_up():
+	TranslationServer.set_locale("en")
 
-func on_german_button_up() -> void:
- TranslationServer.set_locale("de")
+func on_french_button_up():
+	TranslationServer.set_locale("fr")
+
+func on_german_button_up():
+	TranslationServer.set_locale("de")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,12 +1,20 @@
 extends Control
 class_name mainmenu
 
-@onready var language_menu = $language_menu as languagemenu
-@onready var margin = $MarginContainer as MarginContainer
-@onready var play = $MarginContainer/HBoxContainer/VBoxContainer/PLAY as Button
-@onready var quit = $MarginContainer/HBoxContainer/VBoxContainer/QUIT as Button
-@onready var language = $MarginContainer/HBoxContainer/VBoxContainer/LANGUAGE as Button
+#menus stuff
+#@onready var language_menu = $language_menu as languagemenu
+#@onready var margin = $Margin as MarginContainer
+@onready var language_menu = $Margin/LANGUAGE
+@onready var menu = $Margin/Menu
+
+#buttons
+@onready var play = $Margin/Menu/VBoxContainer/PLAY as Button
+@onready var quit = $Margin/Menu/VBoxContainer/QUIT as Button
+@onready var language = $Margin/Menu/VBoxContainer/LANGUAGE as Button
+
 @onready var start_level = preload("res://scenes/game.tscn") as PackedScene
+
+#audio stuff
 @onready var click = $click
 @onready var select = $select
 
@@ -19,26 +27,20 @@ func _ready():
 func on_play() -> void:
 	get_tree().change_scene_to_packed(start_level)
 
-func on_language():
-	print("clicked on languages")
-	margin.visible = false
-	language_menu.set_process(true)
-	language_menu.visible = true
-
 #we press on quit game, we quit
 func on_quit() -> void:
 	get_tree().quit()
 
-func on_exit_menu() -> void:
-	margin.visible = true
-	language_menu.visible = false
+#func on_exit_menu() -> void:
+#	margin.visible = true
+#	language_menu.visible = false
 
 #function to handle signals for the button presses
 func handle_connections() -> void:
 	play.button_up.connect(on_play)
-	language.button_up.connect(on_language)
+	language.button_up.connect(on_language_pressed)
 	quit.button_up.connect(on_quit)
-	language_menu.exit_language_menu.connect(on_exit_menu)
+	#language_menu.exit_language_menu.connect(on_exit_menu)
 
 #function to handle the translation, is set to local language by default
 func translateUI():
@@ -64,3 +66,11 @@ func _on_play_pressed() -> void:
 
 func _on_language_pressed() -> void:
 	click.play()
+
+
+func on_language_pressed() -> void:
+	show_and_hide(language_menu, menu)
+
+func show_and_hide(first, second):
+	first.show()
+	second.hide()
