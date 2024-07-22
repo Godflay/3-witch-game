fc0a84894e379f073874c5c91bf80739286c64ec
gabeadmin <gg.godfreygabriel@gmail.com>
2024-07-21 22:02:48 -0400

    replace translation file for main menu

--- scripts/old_wizard.gd
+++ scripts/old_wizard.gd
@@ -5,8 +5,10 @@ var player_in_area = false
 
 func _process(delta):
 	if player_in_area:
+		#if we haven't met the wizard yet, we go through the campaign dialogue
 		if Input.is_action_just_pressed("e") and wizard_met == false:
 			wizard_meeting()
+		#otherwise, we go through a loop of 3 dialogues to make player progress
 		elif Input.is_action_just_pressed("e") and wizard_met == true:
 			wizard_main_loop()
 
@@ -14,6 +16,7 @@ func run_dialogue(dialogue_string):
 	Dialogic.start(dialogue_string)
 
 func _on_area_2d_body_entered(body):
+	#we added an empty function "player" inside the player script just for this
 	if body.has_method("player"):
 		player_in_area = true
 
